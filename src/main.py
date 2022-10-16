a650e991308dfbe38054eb264e88781651bdd1c7
salvah22 <salvah22@gmail.com>
2022-10-16 18:51:05 +0100

    working proto

--- src/main.py
+++ src/main.py
@@ -1,21 +1,26 @@
 """
-Money Manager software developed by Salvador Hernández, September 2022
+Money Manager software developed by Salvador Hernández
+Created: September 2022
+Last Edit: October 2022
 """
 
-import re
-import pandas as pd
-
+# standard python libraries
+import re, yaml
 import datetime
 from dateutil.relativedelta import relativedelta
-
-from utils.time import parse_period
-from utils.data import data_loader, data_prepare
-from utils.tk_inter import update_tree, popup_tree_window
-
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
 from tkinter.messagebox import showinfo
+
+# extra libraries
+import pandas as pd
+
+# own utils
+from utils.time import parse_period
+from utils.data import data_loader, data_prepare
+from utils.tk_inter import update_tree, popup_tree_window
+
 
 class App:
     """
@@ -38,19 +43,28 @@ class App:
         self.tk_elems['period_months'].set(str(self.period.months) + " months")
         self.tk_elems['period_years'].set(str(self.period.years) + " years")
 
-    def __init__(self, data_path='', tk=True):
+    def __init__(self, tk=True):
         ### parameters ###
-        self.params = {'fonts': {'f08': 'sans-serif, 8', 'f10': 'sans-serif, 10', 'f12': 'sans-serif, 12'}, 'colors': {'green':'#91BF2C', 'blue':'#00ABFF', 'red':'#FF614F'}}
-        # dates in ISO format: '2022-06-01T00:00:00'
+        with open('src/configs/app.yml', 'r') as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
+        with open('user_config.yml', 'r') as f:
+            self.user_config = yaml.load(f, Loader=yaml.FullLoader)
+
+        if self.user_config['primary_currency']:
+            self.config['display_columns'] += [self.user_config['primary_currency']]
+
         self._period = relativedelta(months=1)
-        self.in_out = 'Expenses' # 'Income', 'Expenses', 'Transfer in', 'Transfer out', 'All'
-        self.dates = {'initial': self.todays_month - self._period, 'final': self.todays_month}
+        self.in_out = self.config['default_subset']
+        self.dates = {'initial': self.todays_month - self._period, 'final': self.todays_month} # dates in ISO format: '2022-06-01T00:00:00'
         ### data ###
-        self.data_path = data_path
+        if self.user_config['data_path']:
+            self.data_path = self.user_config['data_path']
+        else:
+            self.data_path = 'src/resources/dummy_data.xlsx'
         self.df = pd.DataFrame()
         self.df_subset = pd.DataFrame()
         self.load_df()
-        print('--- df loaded and prepared ---')
+        print('data loaded and prepared successfully')
         ### tk inter gui related stuff ###
         self.tk_elems = {}
         if tk:
@@ -60,10 +74,7 @@ class App:
             self.tk_elems['main_app'].mainloop()
 
     def load_df(self):
-        if self.data_path != '':
-            self.df = data_loader(self.data_path) # checks for the extensions and employs the proper load function
-        else:
-            self.df = pd.read_excel('/home/salva/personal/budgeting/Mny.Mgr.2022-08-31.xlsx').iloc[::-1]  # inverse order for show
+        self.df = data_loader(self.data_path) # checks for the extensions and employs the proper load function
         # prepare/preprocess the dataframe:
         self.df = data_prepare(self.df)
         # write the dates of the first and last record
@@ -77,70 +88,74 @@ class App:
     def init_tk(self):
         self.tk_elems['main_app'] = tk.Tk()
         self.tk_elems['main_app'].title('Money Mgr.')
-        self.tk_elems['main_app'].geometry('850x500') # 130 per column (6 cols) + 60 idx + 10 margins
+        self.tk_elems['app_width'] = str(len(self.config['display_columns']) * 130 + 60 + 10) # 130 p/column + 60 idx + 10 margins
+        self.tk_elems['main_app'].geometry(self.tk_elems['app_width'] + 'x600')
+        self.tk_elems['icon'] = tk.PhotoImage(file="src/resources/favicon.png")
+        self.tk_elems['main_app'].tk.call('wm', 'iconphoto', self.tk_elems['main_app']._w, self.tk_elems['icon'])
         self.tk_elems['style'] = ttk.Style()
-        self.tk_elems['style'].configure("mystyle.Treeview", font=self.params['fonts']['f08'])
-        self.tk_elems['style'].configure("mystyle.Treeview.Heading", font=self.params['fonts']['f10'])
+        self.tk_elems['style'].configure("mystyle.Treeview", rowheight=30) # default: 20
+        self.tk_elems['style'].configure("mystyle.Treeview", font=self.config['fonts']['f08'])
+        self.tk_elems['style'].configure("mystyle.Treeview.Heading", font=self.config['fonts']['f10'])
         self.tk_elems['style'].layout("mystyle.Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})])
         self.tk_elems['frame_header'] = tk.Frame(self.tk_elems['main_app'])
         self.tk_elems['frame_header'].pack(expand=True)
-        self.tk_elems['frame_tree'] = tk.Frame(self.tk_elems['main_app'], padx=5, pady=5)
-        self.tk_elems['frame_tree'].pack(expand=True)
         self.tk_elems['date_initial'] = tk.StringVar()
         self.tk_elems['date_final'] = tk.StringVar()
         self.tk_elems['period_years'] = tk.StringVar()
         self.tk_elems['period_months'] = tk.StringVar()
         self.tk_elems['period_days'] = tk.StringVar()
-        self.tk_elems['button_today'] = tk.Button(self.tk_elems['frame_header'], text='Today', width=2, command=lambda: self.move_time_window('today'), font=self.params['fonts']['f12'], bg=self.params['colors']['green'])
+        self.tk_elems['button_today'] = tk.Button(self.tk_elems['frame_header'], text='Today', width=2, command=lambda: self.move_time_window('today'), font=self.config['fonts']['f12'], bg=self.config['colors']['green'])
         self.tk_elems['button_today'].pack(side=tk.LEFT)
-        self.tk_elems['button_backwards'] = tk.Button(self.tk_elems['frame_header'], text='<', width=2, command=lambda: self.move_time_window('backwards'), font=self.params['fonts']['f12'])
+        self.tk_elems['button_backwards'] = tk.Button(self.tk_elems['frame_header'], text='<', width=2, command=lambda: self.move_time_window('backwards'), font=self.config['fonts']['f12'])
         self.tk_elems['button_backwards'].pack(side=tk.LEFT)
         ### DROPDOWN: DAILY/WEEKLY/MONTHLY/YEARLY
-        self.tk_elems['entry_date_initial'] = tk.Entry(self.tk_elems['frame_header'], textvariable=self.tk_elems['date_initial'], font=self.params['fonts']['f12'], width=10)
+        self.tk_elems['entry_date_initial'] = tk.Entry(self.tk_elems['frame_header'], textvariable=self.tk_elems['date_initial'], font=self.config['fonts']['f12'], width=10)
         self.tk_elems['entry_date_initial'].pack(side=tk.LEFT)
         self.tk_elems['entry_date_initial'].bind('<Return>', lambda event: self.on_entry_change('initial'))
-        #tk.Label(self.tk_elems['frame_header'], text=" | ", font=self.params['fonts']['f12']).pack(side=tk.LEFT)
-        self.tk_elems['entry_date_final'] = tk.Entry(self.tk_elems['frame_header'], textvariable=self.tk_elems['date_final'], font=self.params['fonts']['f12'], width=10)
+        #tk.Label(self.tk_elems['frame_header'], text=" | ", font=self.config['fonts']['f12']).pack(side=tk.LEFT)
+        self.tk_elems['entry_date_final'] = tk.Entry(self.tk_elems['frame_header'], textvariable=self.tk_elems['date_final'], font=self.config['fonts']['f12'], width=10)
         self.tk_elems['entry_date_final'].pack(side=tk.LEFT)
         self.tk_elems['entry_date_final'].bind('<Return>', lambda event: self.on_entry_change('final'))
-        self.tk_elems['button_onwards'] = tk.Button(self.tk_elems['frame_header'], text='>', width=2, command=lambda: self.move_time_window('onwards'), font=self.params['fonts']['f12'])
+        self.tk_elems['button_onwards'] = tk.Button(self.tk_elems['frame_header'], text='>', width=2, command=lambda: self.move_time_window('onwards'), font=self.config['fonts']['f12'])
         self.tk_elems['button_onwards'].pack(side=tk.LEFT)
         self.tk_elems['frame_period'] = tk.Frame(self.tk_elems['frame_header'])
         self.tk_elems['frame_period'].pack(expand=True, side=tk.LEFT)
-        self.tk_elems['entry_period_years'] = tk.Entry(self.tk_elems['frame_period'], textvariable=self.tk_elems['period_years'], font=self.params['fonts']['f10'], width=8)
+        self.tk_elems['entry_period_years'] = tk.Entry(self.tk_elems['frame_period'], textvariable=self.tk_elems['period_years'], font=self.config['fonts']['f10'], width=8)
         self.tk_elems['entry_period_years'].pack(side=tk.BOTTOM)
         self.tk_elems['entry_period_years'].bind('<Return>', lambda event: self.on_entry_change('period'))
-        self.tk_elems['entry_period_months'] = tk.Entry(self.tk_elems['frame_period'], textvariable=self.tk_elems['period_months'], font=self.params['fonts']['f10'], width=8)
+        self.tk_elems['entry_period_months'] = tk.Entry(self.tk_elems['frame_period'], textvariable=self.tk_elems['period_months'], font=self.config['fonts']['f10'], width=8)
         self.tk_elems['entry_period_months'].pack(side=tk.BOTTOM)
         self.tk_elems['entry_period_months'].bind('<Return>', lambda event: self.on_entry_change('period'))
-        self.tk_elems['entry_period_days'] = tk.Entry(self.tk_elems['frame_period'], textvariable=self.tk_elems['period_days'], font=self.params['fonts']['f10'], width=8)
+        self.tk_elems['entry_period_days'] = tk.Entry(self.tk_elems['frame_period'], textvariable=self.tk_elems['period_days'], font=self.config['fonts']['f10'], width=8)
         self.tk_elems['entry_period_days'].pack(side=tk.BOTTOM)
         self.tk_elems['entry_period_days'].bind('<Return>', lambda event: self.on_entry_change('period'))
         self.tk_elems['frame_in_out'] = tk.Frame(self.tk_elems['frame_header'])
         self.tk_elems['frame_in_out'].pack(expand=True, side=tk.LEFT)
         self.tk_elems['button_income'] = tk.Button(self.tk_elems['frame_in_out'], text='Income', width=4, height=1,
                                                   command=lambda: self.update_subset('Income'),
-                                                  font=self.params['fonts']['f10'], bg=self.params['colors']['blue'])
+                                                  font=self.config['fonts']['f10'], bg=self.config['colors']['blue'])
         self.tk_elems['button_income'].grid(row=0, column=0)
         self.tk_elems['button_expenses'] = tk.Button(self.tk_elems['frame_in_out'], text='Expenses', width=4, height=1,
                                                   command=lambda: self.update_subset('Expenses'),
-                                                  font=self.params['fonts']['f10'], bg=self.params['colors']['red'])
+                                                  font=self.config['fonts']['f10'], bg=self.config['colors']['red'])
         self.tk_elems['button_expenses'].grid(row=1, column=0)
         self.tk_elems['button_transfers'] = tk.Button(self.tk_elems['frame_in_out'], text='Transfer', width=4, height=1,
                                                   command=lambda: self.update_subset('Transfer'),
-                                                  font=self.params['fonts']['f10'])
+                                                  font=self.config['fonts']['f10'])
         self.tk_elems['button_transfers'].grid(row=0, column=1)
         self.tk_elems['button_all'] = tk.Button(self.tk_elems['frame_in_out'], text='All', width=4, height=1,
                                                   command=lambda: self.update_subset('All'),
-                                                  font=self.params['fonts']['f10'])
+                                                  font=self.config['fonts']['f10'])
         self.tk_elems['button_all'].grid(row=1, column=1)
         ### DROPDOWN: EXPENSE/INCOME/TRANSFER
-        self.tk_elems['main_tree'] = ttk.Treeview(self.tk_elems['frame_tree'], style="mystyle.Treeview")
+        self.tk_elems['frame_tree'] = tk.Frame(self.tk_elems['main_app'], padx=5, pady=5)
+        self.tk_elems['frame_tree'].pack(expand=True)
+        self.tk_elems['main_tree'] = ttk.Treeview(self.tk_elems['frame_tree'], style="mystyle.Treeview", height=15) # originally height=10
         self.tk_elems['main_tree'].pack(expand=True)
         self.tk_elems['main_tree'].bind('<Double-1>', self.on_double_click)
         self.tk_elems['frame_footer'] = tk.Frame(self.tk_elems['main_app'])
         self.tk_elems['frame_footer'].pack(expand=True)
-        self.tk_elems['button_groupby_categories'] = tk.Button(self.tk_elems['frame_footer'], text='Groupby categories', width=12, command=self.groupby_categories, font=self.params['fonts']['f10'], bg=self.params['colors']['green'])
+        self.tk_elems['button_groupby_categories'] = tk.Button(self.tk_elems['frame_footer'], text='Groupby categories', width=12, command=self.groupby_categories, font=self.config['fonts']['f10'], bg=self.config['colors']['green'])
         self.tk_elems['button_groupby_categories'].pack(side=tk.LEFT)
 
     def move_time_window(self, direction=''):
@@ -189,7 +204,7 @@ class App:
             self.df_subset = self.df_subset[self.df_subset['Income/Expenses'] == self.in_out]
         elif self.in_out == 'Transfer':
             self.df_subset = self.df_subset[self.df_subset['Income/Expenses'] == 'Transfer in']
-        update_tree(self.df_subset, self.tk_elems['main_tree'])
+        update_tree(self.df_subset, self.tk_elems['main_tree'], self.config['display_columns'])
 
     def on_double_click(self, event):
         #idx = self.tk_elems['main_tree'].selection()[0]
--- src/utils/data.py
+++ src/utils/data.py
@@ -6,24 +6,23 @@ def data_loader(path):
     Function for loading inputs with a variety of extensions with the proper pandas read function
     """
 
-    parent, file = os.path.split(data_path)
+    parent, file = os.path.split(path)
     file_name, file_ext = os.path.splitext(file)
     if file_ext == '.xlsx':
-        df = pd.read_excel(data_path).iloc[::-1]
+        df = pd.read_excel(path)
     elif file_ext == '.csv':
-        df = pd.read_csv(data_path).iloc[::-1]
+        df = pd.read_csv(path)
     else:
         sys.exit('data_path extension not supported')
 
-    return df
+    return df.iloc[::-1] # inverse order for the treeview
 
 
 def data_prepare(df):
     """
     Function for pre-processing the dataframe
     """
-    df['datetime'] = df['Period'] # period is a very bad word for date, use it as datetime
-    df['Period'] = df.apply(lambda row: row['Period'].strftime("%Y-%m-%d"), axis=1) # convert period to strf (YYYY-MM-DD)
-    df.rename(columns={'Period': 'Day'}, inplace=True) # change it to Day
+    df['datetime'] = df['Day'] # period is a very bad word for date, use it as datetime
+    df['Day'] = df.apply(lambda row: row['Day'].strftime("%Y-%m-%d"), axis=1) # convert period to strf (YYYY-MM-DD)
 
     return df
