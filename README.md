869de98a69c1015b96fe7cd1f644c5a21a70f356
salvah22 <salvah22@gmail.com>
2022-11-05 01:18:01 +0000

    working proto, needs refinement in the tree

--- README.md
+++ README.md
@@ -9,7 +9,10 @@ python3 src/main.py '/path/to/data.xlsx/'
 
 # Features
 * A graphical user interface for navigating through your multi-account movements (Expenses, Income, Transfers, etc..), change the period you want to visualize, show per-category totals, and display only the type of transactions you are interested in with just a click.
-* SOON: pie-charts, trends, GUI improvements
+
+![Workflow](.github/img/screenshot1.png)
+
+* SOON: pie-charts, trends
 * SOON: some tools to clean / manipulate your data, adding records
 
 # Dependencies
--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ import pandas as pd
 
 # own utils
 from utils.time import parse_period
-from utils.data import data_loader, data_prepare, get_last_balance_per_account
+from utils.data import *
 from utils.tk_inter import update_tree, popup_tree_window
 
 
@@ -43,12 +43,17 @@ class App:
         self.tk_elems['period_months'].set(str(self.period.months) + " months")
         self.tk_elems['period_years'].set(str(self.period.years) + " years")
 
+    def callbackss(selection):
+        print(selection)
+
     def __init__(self, tk=True):
         ### parameters ###
         with open('src/configs/app.yml', 'r') as f:
             self.config = yaml.load(f, Loader=yaml.FullLoader)
 
         self._period = relativedelta(months=1)
+        self.group = "None"
+        self.group_opts = []
         self.in_out = self.config['default_subset']
         self.dates = {'initial': self.todays_month - self._period, 'final': self.todays_month} # dates in ISO format: '2022-06-01T00:00:00'
         ### data ###
@@ -91,6 +96,7 @@ class App:
         self.df_subset = pd.DataFrame()
 
     def init_tk(self):
+        self.windows = ['main_app']
         self.tk_elems['main_app'] = tk.Tk()
         self.tk_elems['main_app'].title('Money Mgr.')
         self.tk_elems['screen_width'] = self.tk_elems['main_app'].winfo_screenwidth()
@@ -109,6 +115,7 @@ class App:
         self.tk_elems['style'].layout("mystyle.Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})])
         self.tk_elems['frame_header'] = tk.Frame(self.tk_elems['main_app'])
         self.tk_elems['frame_header'].pack(expand=True)
+        ### Period frame
         self.tk_elems['frame_period'] = tk.Frame(self.tk_elems['frame_header'])
         self.tk_elems['frame_period'].pack(expand=True, side=tk.LEFT)
         self.tk_elems['date_initial'] = tk.StringVar()
@@ -120,11 +127,9 @@ class App:
         self.tk_elems['button_today'].pack(side=tk.LEFT)
         self.tk_elems['button_backwards'] = tk.Button(self.tk_elems['frame_period'], text='<', width=2, command=lambda: self.move_time_window('backwards'), font=self.config['fonts']['f12'])
         self.tk_elems['button_backwards'].pack(side=tk.LEFT)
-        ### DROPDOWN: DAILY/WEEKLY/MONTHLY/YEARLY
         self.tk_elems['entry_date_initial'] = tk.Entry(self.tk_elems['frame_period'], textvariable=self.tk_elems['date_initial'], font=self.config['fonts']['f12'], width=10)
         self.tk_elems['entry_date_initial'].pack(side=tk.LEFT)
         self.tk_elems['entry_date_initial'].bind('<Return>', lambda event: self.on_entry_change('initial'))
-        #tk.Label(self.tk_elems['frame_header'], text=" | ", font=self.config['fonts']['f12']).pack(side=tk.LEFT)
         self.tk_elems['entry_date_final'] = tk.Entry(self.tk_elems['frame_period'], textvariable=self.tk_elems['date_final'], font=self.config['fonts']['f12'], width=10)
         self.tk_elems['entry_date_final'].pack(side=tk.LEFT)
         self.tk_elems['entry_date_final'].bind('<Return>', lambda event: self.on_entry_change('final'))
@@ -141,6 +146,7 @@ class App:
         self.tk_elems['entry_period_days'] = tk.Entry(self.tk_elems['frame_period'], textvariable=self.tk_elems['period_days'], font=self.config['fonts']['f10'], width=8)
         self.tk_elems['entry_period_days'].pack(side=tk.BOTTOM)
         self.tk_elems['entry_period_days'].bind('<Return>', lambda event: self.on_entry_change('period'))
+        ### BUTTON GROUP: EXPENSE/INCOME/TRANSFER/ALL
         self.tk_elems['frame_in_out'] = tk.Frame(self.tk_elems['frame_header'])
         self.tk_elems['frame_in_out'].pack(expand=True, side=tk.LEFT)
         self.tk_elems['button_income'] = tk.Button(self.tk_elems['frame_in_out'], text='Income', width=4, height=1,
@@ -159,7 +165,18 @@ class App:
                                                   command=lambda: self.update_subset('All'),
                                                   font=self.config['fonts']['f10'])
         self.tk_elems['button_all'].grid(row=1, column=1)
-        ### DROPDOWN: EXPENSE/INCOME/TRANSFER
+        ### GROUPING
+        self.tk_elems['frame_groups'] = tk.Frame(self.tk_elems['frame_header'], padx=5, pady=5) # , highlightbackground="black", highlightthickness=2
+        self.tk_elems['frame_groups'].pack(expand=True)
+        tk.Label(self.tk_elems['frame_groups'], text="Grouping", font=self.config['fonts']['f10']).pack(side=tk.TOP)
+        self.tk_elems['group'] = tk.StringVar()
+        self.tk_elems['group'].set("None") # default value
+        self.tk_elems['group_opt_menu'] = tk.OptionMenu(self.tk_elems['frame_groups'], self.tk_elems['group'], "None", "Day", "Month", "Year", command=self.group_change)
+        self.tk_elems['group_opt_menu'].pack(side=tk.TOP)
+        self.tk_elems['group_category'] = tk.BooleanVar(value=False)
+        self.tk_elems['checkbox_category'] = ttk.Checkbutton(self.tk_elems['frame_groups'], text='category', variable=self.tk_elems['group_category'], onvalue=True, offvalue=False, command=self.group_opts_change)
+        self.tk_elems['checkbox_category'].pack(side=tk.TOP, expand=True, padx=(0, 5))
+        ### MAIN BIG TREE
         self.tk_elems['frame_tree'] = tk.Frame(self.tk_elems['main_app'], padx=5, pady=5)
         self.tk_elems['frame_tree'].pack(expand=True)
         self.tk_elems['main_tree'] = ttk.Treeview(self.tk_elems['frame_tree'], style="mystyle.Treeview", height=15) # originally height=10
@@ -167,12 +184,23 @@ class App:
         self.tk_elems['main_tree'].bind('<Double-1>', self.on_double_click)
         self.tk_elems['frame_footer'] = tk.Frame(self.tk_elems['main_app'])
         self.tk_elems['frame_footer'].pack(expand=True)
-        self.tk_elems['button_groupby_categories'] = tk.Button(self.tk_elems['frame_footer'], text='Groupby categories', width=12, command=self.groupby_categories, font=self.config['fonts']['f10'], bg=self.config['colors']['green'])
-        self.tk_elems['button_groupby_categories'].pack(side=tk.LEFT)
+        self.tk_elems['button_groupby_category'] = tk.Button(self.tk_elems['frame_footer'], text='Groupby category', width=12, command=self.groupby_category, font=self.config['fonts']['f10'], bg=self.config['colors']['green'])
+        self.tk_elems['button_groupby_category'].pack(side=tk.LEFT)
         if 'AccountBalance' in self.config['display_columns']:
             # show AccountBalance window
             self.show_balances()
-        
+    
+    def group_opts_change(self):
+        if self.tk_elems['group_category'].get():
+            self.group_opts.append('Category')
+        elif 'Category' in self.group_opts:
+            self.group_opts.remove('Category')
+        if self.group != "None":
+            self.update_subset()
+
+    def group_change(self, group):
+        self.group = group
+        self.update_subset()
 
     def move_time_window(self, direction=''):
         if direction == 'today':
@@ -220,7 +248,20 @@ class App:
             self.df_subset = self.df_subset[self.df_subset['Income/Expenses'] == self.in_out]
         elif self.in_out == 'Transfer':
             self.df_subset = self.df_subset[(self.df_subset['Income/Expenses'] == 'Transfer in') | (self.df_subset['Income/Expenses'] == 'Transfer out')]
-        update_tree(self.df_subset, self.tk_elems['main_tree'], self.config['display_columns'])
+        if self.group == 'None':
+            cols = self.config['display_columns']
+        else:
+            cols = ["group"] + self.group_opts
+            if self.group == 'Day':
+                self.df_subset['group'] = self.df_subset['Day']
+            elif self.group == 'Month':
+                self.df_subset['group'] = self.df_subset.apply(lambda row: year_month_from_iso(row['Day']), axis=1)
+            elif self.group == 'Year':
+                self.df_subset['group'] = self.df_subset.apply(lambda row: year_from_iso(row['Day']), axis=1)
+            self.df_subset = self.df_subset.groupby(cols).sum()[self.config['main_currency']].reset_index()
+            # for display:
+            cols += [self.config['main_currency']]
+        update_tree(self.df_subset, self.tk_elems['main_tree'], cols)
 
     def on_double_click(self, event):
         #idx = self.tk_elems['main_tree'].selection()[0]
@@ -247,18 +288,18 @@ class App:
 
         self.update_subset()
 
-    def groupby_categories(self):
+    def groupby_category(self):
         grouped = self.df_subset.groupby(["Category"])['EUR'].sum().reset_index()
         groupedsorted = grouped.sort_values(by='EUR', ascending=True)
         groupedsortedrounded = groupedsorted.round(0)
         popup_tree_window(
             dataframe=groupedsortedrounded,
-            title="Data grouped by categories",
+            title="Data grouped by category",
             icon=self.tk_elems['icon']
         )
 
     def show_balances(self):
-        balances = pd.DataFrame(get_last_balance_per_account(self.df))
+        balances = pd.DataFrame(get_last_balance_per_account(self.df), columns=["Account",self.config['main_currency']])
         position_x = self.tk_elems['main_app_width'] + self.tk_elems['main_app_x'] # width + x (center)
         width = 130 * balances.shape[1] + 60 # 130 p/column + 60 idx + 10 margins ~ 980
         height = 30 * balances.shape[0] + 25
